346cdf2cf4ceadab80ddedf4af99b785f0a1c855
lexanee <ir.muhammadfaiz@gmail.com>
2024-11-24 14:51:16 +0700

    refactor: Update About component with vision, mission, approach, and goals sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,64 +1,74 @@
 import { motion } from "framer-motion";
 import React from "react";
-import Tilt from "react-tilt";
-
-import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { styles } from "../styles";
-import { fadeIn, textVariant } from "../utils/motion";
-
-const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
+import { textVariant } from "../utils/motion";
 
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>Who We Are</h2>
       </motion.div>
-
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
-      </motion.p>
-
-      <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div>
+      <motion.div variants={textVariant()} className="mt-5">
+        <p className={`${styles.sectionSubText} normal-case font-bold`}>
+          Our Vision
+        </p>
+        <p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          We are embarking on an ambitious journey to develop a next-generation
+          digital twin platform. As an emerging player in the industry, we're
+          committed to revolutionizing how organizations manage, visualize, and
+          utilize their operational data.
+        </p>
+      </motion.div>
+      <motion.div variants={textVariant()} className="mt-5">
+        <p className={`${styles.sectionSubText} normal-case font-bold`}>
+          Our Mission
+        </p>
+        <p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          To empower organizations with intelligent data management tools that
+          drive informed decision-making, optimize operations, and promote
+          sustainable practices through innovative digital twin technology.
+        </p>
+      </motion.div>
+      <motion.div className="mt-5">
+        <p className={`${styles.sectionSubText} normal-case font-bold`}>
+          Our Approuch
+        </p>
+        <ul className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] list-disc ml-8">
+          <li>Data First Architecture</li>
+          <li>Intelligent Data Management</li>
+          <li>Ontological Relationships</li>
+          <li>Visualization Excellence</li>
+        </ul>
+      </motion.div>
+      <motion.div className="mt-5">
+        <p className={`${styles.sectionSubText} normal-case font-bold`}>
+          Our Goals
+        </p>
+        <p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          As a new entrant in the digital twin space, we aim to:
+        </p>
+        <ul className="mt-2 text-secondary text-[17px] max-w-3xl leading-[30px] list-disc ml-8">
+          <li>
+            Establish ourselves as an innovative solution provider in the
+            digital twin market.
+          </li>
+          <li>
+            Help organizations minimize environmental impact through better
+            data-driven decisions
+          </li>
+          <li>
+            Create a platform that scales from local operations to global
+            enterprises
+          </li>
+          <li>
+            Build lasting partnerships with industry leaders across
+            diverse sectors
+          </li>
+        </ul>
+      </motion.div>
     </>
   );
 };
