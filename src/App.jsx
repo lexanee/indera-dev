191fe77d3b0aedceba07781627eac1c5be77d633
lexanee <ir.muhammadfaiz@gmail.com>
2024-11-26 12:38:03 +0700

    refactor: Simplify BrowserRouter component usage in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter
-      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
